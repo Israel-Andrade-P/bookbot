51f491e3364fb51c1be79d620348440fc08a306b
Zel <israel88andrade@gmail.com>
2025-06-07 10:13:47 -0300

    add commandline args as input

--- code/main.py
+++ code/main.py
@@ -1,16 +1,26 @@
-from stats import get_word_count, get_character_count
+import stats
+import sys
 
 def get_book_text(file):
     with open(file, "r", encoding="utf-8") as f:
         return f.read()
     
 def main():
-    file_path = "./books/frankenstein.txt"
+    if len(sys.argv) <= 1:
+        print("Usage: py/python/python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
     file_contents = get_book_text(file_path)
-    word_count = get_word_count(file_contents)
-    character_count = get_character_count(file_contents)
+    word_count = stats.get_word_count(file_contents)
+    character_count = stats.get_character_count(file_contents)
+    sorted_chars = stats.get_sorted_list(character_count)
 
-    print(f"{word_count} words found in the document")
-    print(character_count)
+    print(f"\n============ BOOKBOT ============\nAnalyzing book found at {file_path}...\n")
+    print(f"----------- Word Count ----------\nFound {word_count} total words\n")
+    print("--------- Character Count -------")
+    for char_dict in sorted_chars:
+        if char_dict["char"].isalpha():
+            print(f"{char_dict["char"]}:{char_dict["num"]}\n")
+    print("============= END ===============")
 
 main()        
--- code/stats.py
+++ code/stats.py
@@ -5,6 +5,22 @@ def get_character_count(text):
     lower_text = text.lower()
     return {char:lower_text.count(char) for char in lower_text}
 
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_list(my_dict):
+    sorted_list = []
+
+    for key in my_dict:
+        sorted_list.append({"char":key, "num": my_dict[key]})
+
+    sorted_list.sort(reverse=True, key=sort_on)     
+
+    return sorted_list  
+
+
+
+
 
 
        
